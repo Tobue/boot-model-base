b921680865db3c673b6078aacc209b8936a43773
YFU5FJUD5FYCNP5\Administrator <1151814486@qq.com>
2019-09-14 14:32:22 +0800

    Initial commit

---
+++ base-out-api/src/main/java/com/sx/OutApiApplication.java
@@ -0,0 +1,18 @@
+package com.sx;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.sx.service")
+@MapperScan("com.sx.mapper")
+@ComponentScan("com.sx.controller")
+public class OutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutApiApplication.class, args);
+    }
+
+}
